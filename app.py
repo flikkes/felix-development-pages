d75f96638c2d324f38fa4697beb13632bfca69ae
flikkes <lpke.flx@gmail.com>
2021-01-31 21:53:23 +0100

    changed content

--- app.py
+++ app.py
@@ -10,15 +10,15 @@ def home():
     home_caption = "Free database migration tool"
     home_content = Markup(
         """
-        Felix-db-migration-tool is a java based tool we provide for easiely migrating data from one database to another. 
-        In the current version we try to offer a simple API especially for encapsulating data reads and writes from and to a MySQL. 
-        Our goal is to provide a simple and efficient converting mechanism from database 
+        Felix-db-migration-tool is a java based tool for easily migrating data from one database to another. 
+        In the current version I try to offer a simple API especially for encapsulating data reads and writes from and to a MySQL database. 
+        My goal is to provide a simple and efficient converting mechanism from database 
         specific formats into a generic format while covering a large variety of database systems. In the current 
-        version we only support MySQL and MongoDB. Our plan is to enhance functionality and gradually increase the number of supported 
-        database systems. While the free version will continue to receive important updates we'll start to ship an 
-        advanced version of our software which can be purchased. This Version will include advanced functionality, 
+        version I only support MySQL and MongoDB. My plan is to enhance functionality and gradually increase the number of supported 
+        database systems. While the free version will continue to receive important updates I'll start to ship an 
+        advanced version of the software which can be purchased. This Version will include advanced functionality, 
         stable and experimental API and other special features. If you have any questions or want to suggest something 
-        we should include in future updates, feel free do contact us at lpke.flx@gmail.com. 
+        I should include in future updates, feel free to contact me at lpke.flx@gmail.com. 
         The tool can be downloaded <a target="_blank" href="https://github.com/flikkes/felix-db-migration-tool/releases">here</a>.
     """
     )
@@ -32,15 +32,15 @@ def products():
     products_caption = "Free database migration tool"
     products_content = Markup(
         """
-        Felix-db-migration-tool is a java based tool we provide for easiely migrating data from one database to another. 
-        In the current version we try to offer a simple API especially for encapsulating data reads and writes from and to a MySQL. 
-        Our goal is to provide a simple and efficient converting mechanism from database 
+        Felix-db-migration-tool is a java based tool for easily migrating data from one database to another. 
+        In the current version I try to offer a simple API especially for encapsulating data reads and writes from and to a MySQL database. 
+        My goal is to provide a simple and efficient converting mechanism from database 
         specific formats into a generic format while covering a large variety of database systems. In the current 
-        version we only support MySQL and MongoDB. Our plan is to enhance functionality and gradually increase the number of supported 
-        database systems. While the free version will continue to receive important updates we'll start to ship an 
-        advanced version of our software which can be purchased. This Version will include advanced functionality, 
+        version I only support MySQL and MongoDB. My plan is to enhance functionality and gradually increase the number of supported 
+        database systems. While the free version will continue to receive important updates I'll start to ship an 
+        advanced version of the software which can be purchased. This Version will include advanced functionality, 
         stable and experimental API and other special features. If you have any questions or want to suggest something 
-        we should include in future updates, feel free do contact us at lpke.flx@gmail.com. 
+        I should include in future updates, feel free to contact me at lpke.flx@gmail.com. 
         The tool can be downloaded <a target="_blank" href="https://github.com/flikkes/felix-db-migration-tool/releases">here</a>.
     """
     )
