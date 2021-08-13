27d6100b822f1cfcbc3d33caccc9331e9d42e79e
flikkes <lpke.flx@gmail.com>
2021-08-13 20:23:28 +0200

    added disclaimer, changed content

--- app.py
+++ app.py
@@ -13,8 +13,14 @@ def home():
     home_caption = "Felix Development and Consulting"
     home_content = Markup(
         """
-        Welcome to my development and consulting space. 
-        For more information, visit my <a href="/products">Products page</a>. 
+        Hi there,<br><br>
+        my name is Felix. I'm a software engineer from Germany who is passionate about building software which is simple, 
+        straightforward and useful for solving a specific problem. Also I'm highly interested in creating new sources of 
+        knowledge for those who haven't been in touch with the topic of software creation and would like to learn something about it.<br><br>
+        Right now I'm working on two projects which I'd like to turn into actual products within the next months. One being a 
+        java based database migration tool. My second project is a low-threshold programming course which contains fundamentals of 
+        programming, software engineering and creating and deploying your first application.<br><br>
+        For more information, visit <a href="/products">Products</a> and <a href="/courses">Courses</a>. 
         """
     )
     return render_template(
@@ -25,15 +31,15 @@ def home():
 @app.route("/courses")
 def courses():
     active_link = "courses"
-    home_caption = "Felix Development and Consulting"
-    home_content = Markup(
+    courses_caption = "Felix' software development course"
+    courses_content = Markup(
         """
-        Welcome to my development and consulting space. 
-        For more information, visit my <a href="/products">Products page</a>. 
+        A free version of my software development course will be available soon. It will give you a taste of how the content is 
+        structured as well as how things are explained. Be sure to check it out - it's free! 
         """
     )
     return render_template(
-        "index.html", home_caption=home_caption, home_content=home_content, active_link=active_link
+        "index.html", home_caption=courses_caption, home_content=courses_content, active_link=active_link
     )
 
 
