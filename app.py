17603fc11a21084fdf5d3b13673453377e4f73fa
flikkes <lpke.flx@gmail.com>
2022-04-06 21:53:08 +0200

    fixed encoding problem, added data protection announcement

--- app.py
+++ app.py
@@ -9,30 +9,39 @@ if not os.path.isfile("local_dev_environment.flagfile"):
 
 @app.route("/")
 def home():
-    return pages("home")
+    return main_pages("home")
 
 
 @app.route("/<page>")
-def pages(page):
+def main_pages(page):
+    return pages(page,"content", "content", "footer_content")
+
+
+@app.route("/footer-links/<page>")
+def link_pages(page):
+    return pages(page, "footer_content", "content", "footer_content")
+
+
+def pages(page, content_folder, content_links_folder, footer_content_folder):
     pages_caption = "Not found"
     pages_content = "Sorry, no content available."
     active_link = "notfound"
 
     if not page_name_too_long(page):
-        caption_filename = "content/" + page + "_caption.html"
-        content_filename = "content/" + page + ".html"
+        caption_filename = content_folder + "/" + page + "_caption.html"
+        content_filename = content_folder + "/" + page + ".html"
         active_link = page
 
         if os.path.isfile(caption_filename):
-            with open(caption_filename, 'r') as f:
+            with open(caption_filename, 'r', encoding='utf-8') as f:
                 pages_caption = Markup(f.read())
 
         if os.path.isfile(content_filename):
-            with open(content_filename, 'r') as f:
+            with open(content_filename, 'r', encoding='utf-8') as f:
                 pages_content = Markup(f.read())
 
     return render_template(
-        "index.html", caption=pages_caption, content=pages_content, active_link=active_link, items=get_items("content")
+        "index.html", caption=pages_caption, content=pages_content, active_link=active_link, items=get_items(content_links_folder), footer_items=get_items(footer_content_folder)
     )
 
 
